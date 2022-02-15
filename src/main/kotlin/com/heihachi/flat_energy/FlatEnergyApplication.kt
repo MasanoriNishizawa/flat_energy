c8f91c0a37d8e44abb260bc67a2f17ccef8a11c6
masanorinishizawa <57927373+MasanoriNishizawa@users.noreply.github.com>
2022-02-15 11:10:53 +0900

    Initial commit

---
+++ src/main/kotlin/com/heihachi/flat_energy/FlatEnergyApplication.kt
@@ -0,0 +1,11 @@
+package com.heihachi.flat_energy
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class FlatEnergyApplication
+
+fun main(args: Array<String>) {
+	runApplication<FlatEnergyApplication>(*args)
+}
